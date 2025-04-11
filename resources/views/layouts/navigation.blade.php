caebaba347d87dc2f4c6350ea3f448542ca51785
Cesar Junior <cesarmsjunior@gmail.com>
2025-04-11 15:00:24 -0300

    new welcome page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -55,19 +55,27 @@
                     </x-slot>
                 </x-dropdown>
             </div>
+            @else
+            <div class="flex items-center">
+                <a
+                    href="{{ route('login') }}"
+                    class="rounded-md text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 ring-1 ring-transparent transition focus:outline-none">
+                    Log in
+                </a>
+            </div>
             @endauth
 
             <!-- Hamburger -->
+            @auth
             <div class="-me-2 flex items-center sm:hidden">
-                @auth
                 <button id="menuResponsiveBtn" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path class="stack inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path class="x hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
-                @endauth
             </div>
+            @endauth
         </div>
     </div>
 
--- resources/views/poll/basic.blade.php
+++ resources/views/poll/basic.blade.php
@@ -11,7 +11,7 @@
             <p class="mt-1 text-sm/6 text-red-500 dark:text-red-200">
                 {{ __('You need to') }}
                 <a class="content-center underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('vote.login') }}">
-                    {{ __('Login') }}
+                    {{ __('Log in') }}
                 </a>
             </p>
             @endunless
