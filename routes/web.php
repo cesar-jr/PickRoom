773f26ef89a6e06e94e0b75d085881556b7c28c6
Cesar Junior <cesarmsjunior@gmail.com>
2025-04-21 12:44:09 -0300

    remove features that won't be needed going forward

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/polls/list', [PollController::class, 'list'])->name('polls.list');
     Route::get('/polls/my', [PollController::class, 'my'])->name('polls.my');
     Route::post('/vote/{poll}/store', [VoteController::class, 'store'])->name('vote.store');
