31955fc4226947d232ead06e97907a898190a0b8
Cesar Junior <cesarmsjunior@gmail.com>
2025-04-30 14:22:07 -0300

    relative redirect url oauth

--- config/services.php
+++ config/services.php
@@ -38,19 +38,19 @@ return [
     'x' => [
         'client_id' => env('X_CLIENT_ID'),
         'client_secret' => env('X_CLIENT_SECRET'),
-        'redirect' => "http://localhost:8080/auth/x/callback",
+        'redirect' => "/auth/x/callback",
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => "http://localhost:8080/auth/google/callback",
+        'redirect' => "/auth/google/callback",
     ],
 
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => "http://localhost:8080/auth/github/callback",
+        'redirect' => "/auth/github/callback",
     ],
 
 ];
