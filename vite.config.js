2fdfcac66ecba66ec16e22e8cad7cb6e32097c05
Cesar Junior <cesarmsjunior@gmail.com>
2025-05-06 14:17:03 -0300

    vite config fix: all js files on build

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { readdirSync } from "fs";
+import { join } from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 "resources/css/app.css",
-                "resources/js/app.js",
-                ...readdirSync("resources/js/pages").map(
-                    (file) => `resources/js/pages/${file}`
-                ),
+                // "resources/js/app.js",
+                ...readdirSync("resources/js", {
+                    recursive: true,
+                    withFileTypes: true,
+                })
+                    .filter((file) => file.isFile())
+                    .map((file) => join(file.parentPath, file.name)),
             ],
             refresh: true,
         }),
