b46c0b168ce06bfdc91552a52ef2e184820a24be
Cesar Junior <cesarmsjunior@gmail.com>
2025-05-16 19:21:24 -0300

    set trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(InputSanitization::class);
         $middleware->append(SetLocaleByRequest::class);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
